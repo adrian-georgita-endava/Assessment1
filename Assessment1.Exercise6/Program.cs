5d0d29160d2787b19021b2ccf8cc9d0d7bc69988
adrian.georgita <adrian.georgita@endava.com>
2025-10-13 16:26:35 +0300

    Implemented the Multiplications Table

---
+++ Assessment1.Exercise6/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Welcome to the Multiplications Table");
+
+int number;
+
+Console.Write("Please enter a number: ");
+while(!int.TryParse(Console.ReadLine(), out number) || number <= 0)
+{
+    Console.WriteLine("Invalid Number!");
+    Console.Write("Please enter a valid positive number: ");
+}
+
+Console.WriteLine($"Mutliplications Table for X = {number}");
+for(int i = 1; i <= number; i++)
+{
+    for(int j= 1; j <= number; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}");
+    }
+}
