11aab5b5d2f3cb2c36a18469c40905a55b8342d8
adrian.georgita <adrian.georgita@endava.com>
2025-10-13 16:42:20 +0300

    Implemented the Number Guesser Game

---
+++ Assessment1.Exercise7/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Welcome to the Number Guesser");
+
+Random random = new Random();
+int number = random.Next(0, 1000);
+
+int guess = -1;
+int guesses = 0;
+Console.WriteLine("Guess the random number");
+Console.Write("Please enter your guess (0 - 1000): ");
+while(guess != number)
+{
+    Console.Write("Please enter your guess (0 - 1000): ");
+    while (!int.TryParse(Console.ReadLine(), out guess) || guess < 0 || guess > 1000) {
+        Console.WriteLine("Invalid number");
+        Console.Write("Please enter a valid positive number for your guess: ");
+    }
+
+    guesses++;
+    if(guess > number)
+    {
+        Console.WriteLine("Too high");
+    }
+    else if(guess < number)
+    {
+        Console.WriteLine("Too low");
+    }
+    else
+    {
+        Console.WriteLine("Correct!");
+    }
+}
+
+Console.WriteLine($"You have guessesed the number in: {guesses} guessses");
