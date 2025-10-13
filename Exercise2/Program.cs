8df61c999c933e857044a2c3b370a55424f62893
adrian.georgita <adrian.georgita@endava.com>
2025-10-13 15:01:07 +0300

    Implemented the Average Grade Evaluator

---
+++ Exercise2/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Welcome to the Average Grade Evaluator");
+
+int grade = -1;
+int count = 0;
+double avg = 0;
+do
+{
+    Console.Write("Please enter a grade (0 - 100) or '-1' to stop: ");
+    if(int.TryParse(Console.ReadLine(), out grade))
+    {
+        if(grade == -1)
+        {
+            break;
+        }
+
+        if(grade < -1 || grade > 100)
+        {
+            Console.WriteLine("Invalid Grade! Please provide a grade in the range (0 - 100) or '-1' to stop");
+            continue;
+        }
+
+        count++;
+        avg += grade;
+    }
+    else
+    {
+        Console.WriteLine("Invalid Grade! Please provide a grade in the range (0 - 100) or '-1' to stop");
+        continue;
+    }
+} while (grade != -1);
+
+if (count > 0)
+{
+    Console.WriteLine($"Average Grade: {avg / count}");
+}
+else
+{
+    Console.WriteLine("No grades provided");
+}
