679408c1e4bddd450ff7876fe573e0a534a83c49
adrian.georgita <adrian.georgita@endava.com>
2025-10-14 14:02:41 +0300

    Implemented the Sequence Generator

---
+++ Assessment1.Exercise8/Program.cs
@@ -0,0 +1,76 @@
+﻿using Assessment1.Exercise8;
+
+Console.WriteLine("Welcome to the Sequence Generator");
+
+int numberOfTerms;
+Console.Write("Enter the number of terms to generate: ");
+while(!int.TryParse(Console.ReadLine(), out numberOfTerms) || numberOfTerms < 1)
+{
+    Console.WriteLine("Invalid number. Please enter a positive number");
+    Console.Write("Enter the number of terms to generate: ");
+}
+
+string[] validSequences = ["fibonacci", "geometric", "prime", "factorial", "triangular", "perfect squares"];
+
+Console.WriteLine("Sequence Types: 'Fibonacci', 'Geometric', 'Prime', 'Factorial' 'Trangular', 'Perfect Squares'");
+Console.Write("Enter the type of sequence: ");
+string? sequenceName = Console.ReadLine();
+while (String.IsNullOrEmpty(sequenceName) || !validSequences.Contains(sequenceName.ToLower()))
+{
+    Console.WriteLine("Invalid Sequence Type");
+    Console.WriteLine("Sequence Types: 'Fibonacci', 'Geometric', 'Prime', 'Factorial' 'Trangular', 'Perfect Squares'");
+    Console.Write("Enter the type of sequence: ");
+    sequenceName = Console.ReadLine();
+}
+
+
+sequenceName = sequenceName.ToLower();
+
+try
+{
+    switch (sequenceName)
+    {
+        case "fibonacci":
+            Sequence.PrintFibonacci(numberOfTerms);
+            break;
+        case "geometric":
+            int firstTerm, ratio;
+            Console.Write("Enter the first term of the geometric sequence: ");
+            while (!int.TryParse(Console.ReadLine(), out firstTerm))
+            {
+                Console.WriteLine("Invalid Value. Please enter a valid positive numeric value!");
+                Console.Write("Enter the first term of the geometric sequence: ");
+            }
+
+            Console.Write("Enter the ratio of the geometric sequence: ");
+            while (!int.TryParse(Console.ReadLine(), out ratio))
+            {
+                Console.WriteLine("Invalid Value. Please enter a valid positive numeric value!");
+                Console.Write("Enter the ratio of the geometric sequence: ");
+            }
+
+            Sequence.PrintGeometric(numberOfTerms, firstTerm, ratio);
+
+            break;
+        case "prime":
+            Sequence.PrintPrimes(numberOfTerms);
+            break;
+        case "factorial":
+            Sequence.PrintFactorial(numberOfTerms);
+            break;
+        case "triangular":
+            Sequence.PrintTriangular(numberOfTerms);
+            break;
+        case "perfect squares":
+            Sequence.PrintPerfectSquares(numberOfTerms);
+            break;
+        default:
+            Console.WriteLine("Invalid Sequence Type!");
+            break;
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine();
+    Console.WriteLine(e.Message);
+}
---
+++ Assessment1.Exercise8/Sequence.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment1.Exercise8
+{
+    public static class Sequence
+    {
+        public static void PrintFibonacci(int numberOfTerms)
+        {
+            Console.WriteLine("Fibonacci Sequence");
+
+            long f1 = 0;
+            long f2 = 1;
+
+            if (numberOfTerms >= 1)
+            {
+                Console.Write($"{f1} ");
+            }
+
+            if(numberOfTerms >= 2)
+            {
+                Console.Write($"{f2} ");
+            }
+
+            if(numberOfTerms > 2)
+            {
+                for (int i = 2; i < numberOfTerms; i++)
+                {
+                    long f3 = f1 + f2;
+                    Console.Write($"{f3} ");
+                    f1 = f2;
+                    f2 = f3;
+                }
+            }
+        }
+
+        public static void PrintGeometric(int numberOfTerms, int firstTerm, int ratio)
+        {
+            Console.WriteLine("Geometric Sequence");
+
+            long r = ratio;
+            long a = firstTerm;
+
+            if(numberOfTerms >= 1)
+            {
+                Console.Write($"{a} + ");
+            }
+
+            for (int i = 1; i < numberOfTerms; ++i)
+            {
+                checked
+                {
+                    Console.Write($"{a * r}");
+                    r *= ratio;
+                }
+
+                if(i !=  numberOfTerms - 1)
+                {
+                    Console.Write(" + ");
+                }
+            }
+        }
+
+        public static void PrintPrimes(int numberOfTerms)
+        {
+            Console.WriteLine("Primes Sequence");
+
+            if (numberOfTerms >= 1)
+            {
+                Console.Write("2 ");
+            }
+
+            long x = 3;
+            for(int i = 1; i < numberOfTerms; i++)
+            {
+                bool foundPrime = false;
+                while(!foundPrime)
+                {
+                    bool isPrime = true;
+                    for(int d=2; d*d <= x; d++)
+                    {
+                        if(x%d == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+
+                    if(isPrime)
+                    {
+                        foundPrime = true;
+                        Console.Write($"{x} ");
+                    }
+
+                    x++;
+                }
+            }
+        }
+
+        public static void PrintFactorial(int numberOfTerms)
+        {
+            Console.WriteLine("Factorial Sequence");
+
+            long number = 1;
+            for(int i=1; i <= numberOfTerms; ++i)
+            {
+                number *= i;
+                Console.Write($"{number} ");
+            }
+        }
+
+        public static void PrintPerfectSquares(int numberOfTerms)
+        {
+            Console.WriteLine("Perfect Squares Sequence");
+
+            for (long i=1; i<=numberOfTerms; ++i)
+            {
+                Console.Write($"{i * i} ");
+            }
+        }
+
+        public static void PrintTriangular(int numberOfTerms)
+        {
+            Console.WriteLine("Triangular Sequence");
+            for(int i = 1; i <= numberOfTerms; i++)
+            {
+                Console.Write($"{(i * (i + 1)) / 2} ");
+            }
+        }
+    }
+}
