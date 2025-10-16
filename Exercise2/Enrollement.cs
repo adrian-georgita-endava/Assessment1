558a4e53c52493cf9238cc8a2d0b1273558efd7c
adrian.georgita <adrian.georgita@endava.com>
2025-10-16 14:26:39 +0300

    Refactored to use classes.

---
+++ Exercise2/Enrollement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment1.Exercise2
+{
+    public class Enrollement
+    {
+        public int StudentId { get; init; }
+        public Student Student { get; init; }
+        
+        public string CourseCode { get; init; } = String.Empty;
+        public Course Course { get; init; }
+
+        public List<int> Grades { get; } = new List<int>();
+
+        public Enrollement(Student student, Course course)
+        {
+            Student = student;
+            StudentId = student.Id;
+
+            Course = course;
+            CourseCode = course.Code;
+        }
+
+        public int GetAverageGrade()
+        {
+            return Grades.Count > 0 ? (int) Grades.Average(x => x) : 0;
+        }
+    }
+}
---
+++ Exercise2/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment1.Exercise2
+{
+    public class Student
+    {
+        public int Id { get; init; }
+        public string FirstName { get; init; } = String.Empty;
+        public string LastName { get; init; } = String.Empty;
+        public List<Enrollement> Enrollements { get; } = new();
+
+        private static int _currentId = 0;
+
+        public Student(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = _currentId++;
+        }
+
+        public int GetAverageGrade()
+        {
+            return (int) Enrollements.Where(e => e.Grades.Count > 0).Average(e => e.GetAverageGrade());
+        }
+
+        public int GetAverageCourseGrade(string courseCode)
+        {
+            Enrollement? enrollement = Enrollements.FirstOrDefault(e => e.CourseCode == courseCode);
+            if (enrollement == null)
+                throw new Exception("Student is not enrolled to this course");
+
+            return enrollement.GetAverageGrade();
+        }
+
+        public void Enroll(Course course)
+        {
+            Enrollements.Add(new Enrollement(this, course));
+        }
+    }
+}
