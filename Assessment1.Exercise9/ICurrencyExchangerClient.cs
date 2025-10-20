6495cfc231f5aaf5048f80b6c40bb0c8e81e3515
adrian.georgita <adrian.georgita@endava.com>
2025-10-20 14:14:12 +0300

    Implemented the currency exchanger

---
+++ Assessment1.Exercise9/ICurrencyExchangerClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment1.Exercise9
+{
+    public interface ICurrencyExchangerClient
+    {
+        public Task<Dictionary<string, Dictionary<string, double>>?> GetConversionRates(string baseCurrency, DateOnly startDate, DateOnly endDate);
+
+        public void Display(double baseValue, Dictionary<string, Dictionary<string, double>> conversionRates);
+    }
+}
---
+++ Assessment1.Exercise9/Program.cs
@@ -0,0 +1,29 @@
+﻿using Assessment1.Exercise9;
+
+Console.WriteLine("Welcome to the Currency Exchange");
+
+Console.WriteLine("Please enter the currency amount in RON");
+double amount;
+Console.Write("Amount: ");
+while(!double.TryParse(Console.ReadLine(), out amount) || amount < 0)
+{
+    Console.WriteLine("Please enter a valid value!");
+    Console.Write("Amount: ");
+}
+
+HttpClient httpClient = new HttpClient();
+
+DateOnly startDate = DateOnly.FromDateTime(DateTime.Now).AddDays(-30);
+DateOnly endDate = DateOnly.FromDateTime(DateTime.Now);
+
+ICurrencyExchangerClient exchangeClient = new FrankfurterCurrencyExchangeClient(httpClient);
+Dictionary<string, Dictionary<string, double>>? exchangeRates = await exchangeClient.GetConversionRates("RON", startDate, endDate);
+
+if(exchangeRates != null)
+{
+    exchangeClient.Display(amount, exchangeRates);
+}
+else
+{
+    Console.WriteLine("Failed to get exchange rates!");
+}
