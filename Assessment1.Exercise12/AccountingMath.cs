f8a89d1953c1a4d464de12704ce3cb0a4bde7d89
adrian.georgita <adrian.georgita@endava.com>
2025-10-22 14:46:03 +0300

    Implemented the Accounting Safe Divide

---
+++ Assessment1.Exercise12/AccountingMath.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment1.Exercise12
+{
+    public static class AccountingMath
+    {
+        public static decimal SafeDivide(decimal numerator, decimal denominator, int decimals)
+        {
+            if(denominator == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by 0");
+            }
+
+            decimal result = numerator / denominator;
+
+            return Math.Round(result, decimals, MidpointRounding.AwayFromZero);
+        }
+    }
+}
