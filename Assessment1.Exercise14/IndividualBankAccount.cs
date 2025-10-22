ab9e022af179e77cc473bc3b2e9a2f424b66cc18
adrian.georgita <adrian.georgita@endava.com>
2025-10-22 17:47:11 +0300

    Implemented the Bank Account Simulator

---
+++ Assessment1.Exercise14/IndividualBankAccount.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment1.Exercise14
+{
+    public class IndividualBankAccount : IBankAccount
+    {
+        public int AccountNumber { get; }
+        public decimal Balance { get; set; }
+        public IBankCustomer Customer { get; }
+
+        private static int _currentAccountNumber = 0;
+
+        public IndividualBankAccount(IBankCustomer customer)
+        {
+            Customer = customer;
+            AccountNumber = _currentAccountNumber++;
+        }
+
+        public void Deposit(decimal amount) => Balance += (amount > 0 ? amount : 0);
+
+        public bool Withdraw(decimal amount)
+        {
+            if(Balance < amount)
+            {
+                return false;
+            }
+
+            Balance -= amount;
+            return true;
+        }
+        public bool Transfer(IBankAccount account, decimal amount)
+        {
+            if (Balance < amount)
+            {
+                return false;
+            }
+
+            account.Deposit(amount);
+            this.Withdraw(amount);
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return $"[Bank Account: {AccountNumber}] Balance: {Balance}";
+        }
+    }
+}
---
+++ Assessment1.Exercise14/IndividualBankCustomer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment1.Exercise14
+{
+    public class IndividualBankCustomer : IBankCustomer
+    {
+        public string Name { get; set; }
+        public int CustomerId { get; }
+
+        private string _firstName;
+        private string _lastName;
+
+        private static int _currentCustomerId = 0;
+
+        public IndividualBankCustomer(string firstName, string lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            Name = $"{_firstName} {_lastName}";
+            CustomerId = _currentCustomerId++;
+        }
+
+        public override string ToString()
+        {
+            return $"[{CustomerId}] {Name}";
+        }
+    }
+}
